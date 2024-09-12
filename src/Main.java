5824c7efb93c293810b70b542fb77cb88e7b0a09
PETER MUYOMBA <111797748+pmuyomba11@users.noreply.github.com>
2024-09-12 10:21:03 -0400

    Data types Int, Long, Float, Long added.

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 12;  //integers....NOTE: they do have a certain limit.
+        long b = 34455566467888L; //Long ...NOTE: they do have a certain limit. Add an L at the end
+        float c = 2.5F; //float. Always add an F at the end. It also has a limit.
+        double d = 4565.5645736567; // Double...It is limitless
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+    }
+}
